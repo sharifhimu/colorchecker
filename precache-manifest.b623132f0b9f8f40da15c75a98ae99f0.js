d966619eff779100bef889f3005e7ee125116b83
sharifhimu <sharifsorotcu@gmail.com>
2020-06-03 22:07:40 +0600

    Updates

---
+++ precache-manifest.b623132f0b9f8f40da15c75a98ae99f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab88aee6b209c1de5ae366459bce18e",
+    "url": "/colorchecker/index.html"
+  },
+  {
+    "revision": "d29b1508416459b8695d",
+    "url": "/colorchecker/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "92d29ad48921c9b2cb90",
+    "url": "/colorchecker/static/css/main.4df0791a.chunk.css"
+  },
+  {
+    "revision": "d29b1508416459b8695d",
+    "url": "/colorchecker/static/js/2.107c3a7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colorchecker/static/js/2.107c3a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d29ad48921c9b2cb90",
+    "url": "/colorchecker/static/js/main.dc771f72.chunk.js"
+  },
+  {
+    "revision": "c3bca879828336ae5803",
+    "url": "/colorchecker/static/js/runtime-main.bdd75b1f.js"
+  },
+  {
+    "revision": "736d1382987f04e4f671bce51ed6e6c2",
+    "url": "/colorchecker/static/media/colorchecker.736d1382.png"
+  },
+  {
+    "revision": "d24a5895fe92226bfb153636ca8011bd",
+    "url": "/colorchecker/static/media/icon.d24a5895.png"
+  }
+]);
